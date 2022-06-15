97223a3ed24ec84f7a50ea5e566ee0a00398f9cd
LAPTOP-BL5AR6FO\mrkra <mrkrajkumar212@gmail.com>
2022-06-15 14:01:04 -0400

    Initial SetUP

---
+++ navigation/tabs.js
@@ -0,0 +1,164 @@
+import React from 'react';
+import { View, Image, TouchableOpacity } from 'react-native';
+import { createBottomTabNavigator, BottomTabBar } from "@react-navigation/bottom-tabs"
+
+import { Home } from "../screens"
+
+import { COLORS, icons } from "../constants"
+
+const Tab = createBottomTabNavigator();
+
+
+//custom styling on the bottom tag navigator
+const TabBarCustomButton = ({ accessibilityState, children, onPress }) => {
+    if (accessibilityState.selected) {
+        return (
+            <View style={{ flex: 1, alignItems: "center" }}>
+                <View style={{ flexDirection: 'row', position: 'absolute', top: 0 }}>
+                    <View style={{ flex: 1, backgroundColor: COLORS.white }}></View>
+                </View>
+
+                <TouchableOpacity
+                    style={{
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        width: 50,
+                        height: 50,
+                        borderRadius: 25,
+                        backgroundColor: COLORS.white
+                    }}
+                    onPress={onPress}
+                >
+                    {children}
+                </TouchableOpacity>
+            </View>
+        )
+    } else {
+        return (
+            <TouchableOpacity
+                style={{
+                    flex: 1,
+                    height: 60,
+                    backgroundColor: COLORS.white
+                }}
+                activeOpacity={1}
+                onPress={onPress}
+            >
+                {children}
+            </TouchableOpacity>
+        )
+    }
+}
+
+// bottom navigator tabs.
+const Tabs = () => {
+    return (
+        <Tab.Navigator
+            tabBarOptions={{
+                showLabel: false,
+                style: {
+                    position: 'absolute',
+                    left: 0,
+                    bottom: 0,
+                    right: 0,
+                    borderTopWidth: 0,
+                    elevation: 0
+                }
+            }}
+        >
+            <Tab.Screen
+                name="Home"
+                component={Home}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <Image
+                            source={icons.cutlery}
+                            resizeMode="contain"
+                            style={{
+                                width: 25,
+                                height: 25,
+                                tintColor: focused ? COLORS.primary : COLORS.secondary
+                            }}
+                        />
+                    ),
+                    tabBarButton: (props) => (
+                        <TabBarCustomButton
+                            {...props}
+                        />
+                    )
+                }}
+            />
+
+            <Tab.Screen
+                name="Search"
+                component={Home}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <Image
+                            source={icons.search}
+                            resizeMode="contain"
+                            style={{
+                                width: 25,
+                                height: 25,
+                                tintColor: focused ? COLORS.primary : COLORS.secondary
+                            }}
+                        />
+                    ),
+                    tabBarButton: (props) => (
+                        <TabBarCustomButton
+                            {...props}
+                        />
+                    )
+                }}
+            />
+
+            <Tab.Screen
+                name="Like"
+                component={Home}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <Image
+                            source={icons.like}
+                            resizeMode="contain"
+                            style={{
+                                width: 25,
+                                height: 25,
+                                tintColor: focused ? COLORS.primary : COLORS.secondary
+                            }}
+                        />
+                    ),
+                    tabBarButton: (props) => (
+                        <TabBarCustomButton
+                            {...props}
+                        />
+                    )
+                }}
+            />
+
+            <Tab.Screen
+                name="User"
+                component={Home}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <Image
+                            source={icons.user}
+                            resizeMode="contain"
+                            style={{
+                                width: 25,
+                                height: 25,
+                                tintColor: focused ? COLORS.primary : COLORS.secondary
+                            }}
+                        />
+                    ),
+                    tabBarButton: (props) => (
+                        <TabBarCustomButton
+                            {...props}
+                        />
+                    )
+                }}
+            />
+        </Tab.Navigator>
+    )
+}
+
+export default Tabs
---
+++ screens/Home.js
@@ -0,0 +1,496 @@
+import React from "react";
+import {
+    SafeAreaView,
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    Image,
+    FlatList
+} from "react-native";
+
+import { icons, images, SIZES, COLORS, FONTS } from '../constants'
+
+const Home = ({ navigation }) => {
+
+    // Dummy Datas
+
+    const initialCurrentLocation = {
+        streetName: "Morristown",
+        gps: {
+            latitude: 1.5496614931250685,
+            longitude: 110.36381866919922
+        }
+    }
+
+    const categoryData = [
+        {
+            id: 1,
+            name: "Restaurent",
+        },
+        {
+            id: 2,
+            name: "Indoor activity",
+        },
+        {
+            id: 3,
+            name: "Outdoor activity",
+        },
+        {
+            id: 4,
+            name: "Concerts/Events",
+        },
+        {
+            id: 5,
+            name: "Bar",
+        },
+        {
+            id: 6,
+            name: "Spa",
+        }
+    ]
+
+    // price rating
+    const affordable = 1
+    const fairPrice = 2
+    const expensive = 3
+
+    const restaurantData = [
+        {
+            id: 1,
+            name: "The Frog Restaurant and Sports Bar",
+            rating: 4.8,
+            categories: [5, 7],
+            priceRating: affordable,
+            photo: images.burger_restaurant_1,
+            location: {
+                latitude: 40.7970149,
+                longitude: -74.4827253
+            },
+            courier: {
+                avatar: images.avatar_1,
+                name: "Amy"
+            }
+        },
+        {
+            id: 2,
+            name: "Grasshopper off the Green",
+            rating: 4.2,
+            categories: [2, 4, 6],
+            priceRating: expensive,
+            photo: images.pizza_restaurant,
+            duration: "15 - 20 min",
+            location: {
+                latitude: 40.7971935,
+                longitude: -74.4784293
+            }
+        },
+        {
+            id: 3,
+            name: "ByProgrammers Hotdogs",
+            rating: 4.8,
+            categories: [3],
+            priceRating: expensive,
+            photo: images.hot_dog_restaurant,
+            duration: "20 - 25 min",
+            location: {
+                latitude: 1.5238753474714375,
+                longitude: 110.34261833833622,
+            },
+            courier: {
+                avatar: images.avatar_3,
+                name: "James"
+            }
+        },
+        {
+            id: 4,
+            name: "ByProgrammers Sushi",
+            rating: 4.8,
+            categories: [8],
+            priceRating: expensive,
+            photo: images.japanese_restaurant,
+            duration: "10 - 15 min",
+            location: {
+                latitude: 1.5578068150528928,
+                longitude: 110.35482523764315,
+            },
+            courier: {
+                avatar: images.avatar_4,
+                name: "Ahmad"
+            },
+            menu: [
+                {
+                    menuId: 9,
+                    name: "Sushi sets",
+                    photo: images.sushi,
+                    description: "Fresh salmon, sushi rice, fresh juicy avocado",
+                    calories: 100,
+                    price: 50
+                }
+            ]
+        },
+        {
+            id: 5,
+            name: "ByProgrammers Cuisine",
+            rating: 4.8,
+            categories: [1, 2],
+            priceRating: affordable,
+            photo: images.noodle_shop,
+            duration: "15 - 20 min",
+            location: {
+                latitude: 1.558050496260768,
+                longitude: 110.34743759630511,
+            },
+            courier: {
+                avatar: images.avatar_4,
+                name: "Muthu"
+            },
+            menu: [
+                {
+                    menuId: 10,
+                    name: "Kolo Mee",
+                    photo: images.kolo_mee,
+                    description: "Noodles with char siu",
+                    calories: 200,
+                    price: 5
+                },
+                {
+                    menuId: 11,
+                    name: "Sarawak Laksa",
+                    photo: images.sarawak_laksa,
+                    description: "Vermicelli noodles, cooked prawns",
+                    calories: 300,
+                    price: 8
+                },
+                {
+                    menuId: 12,
+                    name: "Nasi Lemak",
+                    photo: images.nasi_lemak,
+                    description: "A traditional Malay rice dish",
+                    calories: 300,
+                    price: 8
+                },
+                {
+                    menuId: 13,
+                    name: "Nasi Briyani with Mutton",
+                    photo: images.nasi_briyani_mutton,
+                    description: "A traditional Indian rice dish with mutton",
+                    calories: 300,
+                    price: 8
+                },
+
+            ]
+        },
+        {
+
+            id: 6,
+            name: "ByProgrammers Dessets",
+            rating: 4.9,
+            categories: [9, 10],
+            priceRating: affordable,
+            photo: images.kek_lapis_shop,
+            duration: "35 - 40 min",
+            location: {
+                latitude: 1.5573478487252896,
+                longitude: 110.35568783282145,
+            },
+            courier: {
+                avatar: images.avatar_1,
+                name: "Jessie"
+            },
+            menu: [
+                {
+                    menuId: 12,
+                    name: "Teh C Peng",
+                    photo: images.teh_c_peng,
+                    description: "Three Layer Teh C Peng",
+                    calories: 100,
+                    price: 2
+                },
+                {
+                    menuId: 13,
+                    name: "ABC Ice Kacang",
+                    photo: images.ice_kacang,
+                    description: "Shaved Ice with red beans",
+                    calories: 100,
+                    price: 3
+                },
+                {
+                    menuId: 14,
+                    name: "Kek Lapis",
+                    photo: images.kek_lapis,
+                    description: "Layer cakes",
+                    calories: 300,
+                    price: 20
+                }
+            ]
+
+        }
+
+
+    ]
+
+    const [categories, setCategories] = React.useState(categoryData)
+    const [selectedCategory, setSelectedCategory] = React.useState(null)
+    const [restaurants, setRestaurants] = React.useState(restaurantData)
+    const [currentLocation, setCurrentLocation] = React.useState(initialCurrentLocation)
+
+
+    function onSelectCategory(category) {
+        //filter restaurant
+        let restaurantList = restaurantData.filter(a => a.categories.includes(category.id))
+
+        setRestaurants(restaurantList)
+
+        setSelectedCategory(category)
+    }
+
+    function getCategoryNameById(id) {
+        let category = categories.filter(a => a.id == id)
+
+        if (category.length > 0)
+            return category[0].name
+
+        return ""
+    }
+
+    function renderHeader() {
+        return (
+            <View style={{ flexDirection: 'row', height: 50 }}>
+                <TouchableOpacity
+                    style={{
+                        width: 50,
+                        paddingLeft: SIZES.padding * 2,
+                        justifyContent: 'center'
+                    }}
+                >
+                    <Image
+                        source={icons.nearby}
+                        resizeMode="contain"
+                        style={{
+                            width: 30,
+                            height: 30
+                        }}
+                    />
+                </TouchableOpacity>
+
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <View
+                        style={{
+                            width: '70%',
+                            height: "100%",
+                            backgroundColor: COLORS.lightGray3,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            borderRadius: SIZES.radius
+                        }}
+                    >
+                        <Text style={{ ...FONTS.h3 }}>{currentLocation.streetName}</Text>
+                    </View>
+                </View>
+
+                <TouchableOpacity
+                    style={{
+                        width: 50,
+                        paddingRight: SIZES.padding * 2,
+                        justifyContent: 'center'
+                    }}
+                >
+                    <Image
+                        source={icons.basket}
+                        resizeMode="contain"
+                        style={{
+                            width: 30,
+                            height: 30
+                        }}
+                    />
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
+    function renderMainCategories() {
+        const renderItem = ({ item }) => {
+            return (
+                <TouchableOpacity
+                    style={{
+                        padding: SIZES.padding,
+                        backgroundColor: (selectedCategory?.id == item.id) ? COLORS.primary : COLORS.white,
+                        borderRadius: SIZES.radius,
+                        alignItems: "center",
+                        justifyContent: "center",
+                        marginRight: SIZES.padding,
+                        ...styles.shadow
+                    }}
+                    onPress={() => onSelectCategory(item)}
+                >
+
+                    <Text
+                        style={{
+                            color: (selectedCategory?.id == item.id) ? COLORS.white : COLORS.black,
+                            ...FONTS.body5
+                        }}
+                    >
+                        {item.name}
+                    </Text>
+                </TouchableOpacity>
+            )
+        }
+
+        return (
+            <View style={{ padding: SIZES.padding * 2 }}>
+                <Text style={{ ...FONTS.h1 }}>Categories</Text>
+                <FlatList
+                    data={categories}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    keyExtractor={item => `${item.id}`}
+                    renderItem={renderItem}
+                    contentContainerStyle={{ paddingVertical: SIZES.padding * 1}}
+                />
+            </View>
+        )
+    }
+
+    function renderRestaurantList() {
+        const renderItem = ({ item }) => (
+            <TouchableOpacity
+                style={{ marginBottom: SIZES.padding * 2 }}
+                onPress={() => navigation.navigate("Restaurant", {
+                    item,
+                    currentLocation
+                })}
+            >
+                {/* Image */}
+                <View
+                    style={{
+                        marginBottom: SIZES.padding
+                    }}
+                >
+                    <Image
+                        source={item.photo}
+                        resizeMode="cover"
+                        style={{
+                            width: "100%",
+                            height: 200,
+                            borderRadius: SIZES.radius
+                        }}
+                    />
+
+                    <View
+                        style={{
+                            position: 'absolute',
+                            bottom: 0,
+                            height: 50,
+                            width: SIZES.width * 0.3,
+                            backgroundColor: COLORS.white,
+                            borderTopRightRadius: SIZES.radius,
+                            borderBottomLeftRadius: SIZES.radius,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            ...styles.shadow
+                        }}
+                    >
+                        <Text style={{ ...FONTS.h4 }}>{item.duration}</Text>
+                    </View>
+                </View>
+
+                {/* Restaurant Info */}
+                <Text style={{ ...FONTS.body2 }}>{item.name}</Text>
+
+                <View
+                    style={{
+                        marginTop: SIZES.padding,
+                        flexDirection: 'row'
+                    }}
+                >
+                    {/* Rating */}
+                    <Image
+                        source={icons.star}
+                        style={{
+                            height: 20,
+                            width: 20,
+                            tintColor: COLORS.primary,
+                            marginRight: 10
+                        }}
+                    />
+                    <Text style={{ ...FONTS.body3 }}>{item.rating}</Text>
+
+                    {/* Categories */}
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            marginLeft: 10
+                        }}
+                    >
+                        {
+                            item.categories.map((categoryId) => {
+                                return (
+                                    <View
+                                        style={{ flexDirection: 'row' }}
+                                        key={categoryId}
+                                    >
+                                        <Text style={{ ...FONTS.body3 }}>{getCategoryNameById(categoryId)}</Text>
+                                        <Text style={{ ...FONTS.h3, color: COLORS.darkgray }}> . </Text>
+                                    </View>
+                                )
+                            })
+                        }
+
+                        {/* Price */}
+                        {
+                            [1, 2, 3].map((priceRating) => (
+                                <Text
+                                    key={priceRating}
+                                    style={{
+                                        ...FONTS.body3,
+                                        color: (priceRating <= item.priceRating) ? COLORS.black : COLORS.darkgray
+                                    }}
+                                >$</Text>
+                            ))
+                        }
+                    </View>
+                </View>
+            </TouchableOpacity>
+        )
+
+        return (
+            <FlatList
+                data={restaurants}
+                keyExtractor={item => `${item.id}`}
+                renderItem={renderItem}
+                contentContainerStyle={{
+                    paddingHorizontal: SIZES.padding * 2,
+                    paddingBottom: 30
+                }}
+            />
+        )
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            {renderHeader()}
+            {renderMainCategories()}
+            {renderRestaurantList()}
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.lightGray4
+    },
+    shadow: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+        elevation: 1,
+    }
+})
+
+export default Home
