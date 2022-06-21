70eb4936e6c6bc7504e9f04893d3aea2dc08e7ea
LAPTOP-BL5AR6FO\mrkra <mrkrajkumar212@gmail.com>
2022-06-21 14:32:25 -0400

    Display Items ready

--- screens/home/Home.js
+++ screens/home/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"; 
+import React, { useEffect, useState } from "react";
 import {
     SafeAreaView,
     View,
@@ -17,29 +17,29 @@ import styles from "./style";
 const Home = ({ navigation }) => {
 
     const url2 = "https://partner-api.groupon.com/deals.json?tsToken=US_AFF_0_6162194_212556_0&lat=40.7954198&lng=-74.4794881&radius=1&filters=category:things-to-do";
-    const url ="http://192.168.1.199:8080/data/exploreAll?lat=40.7957927&lng=-74.4801174&category=things-to-do&range=10";
+    const url = "http://192.168.1.199:8080/data/exploreAll?lat=40.7957927&lng=-74.4801174&category=things-to-do&range=10";
     const url1 = "http://172.28.48.1:8080/data/exploreAll?lat=40.7957927&lng=-74.4801174&category=things-to-do&range=10";
 
-   const[explorerData, setExplorerData] = useState();
-   const[isLoaded, setIsLoaded] = useState(true);
+    const [explorerData, setExplorerData] = useState();
+    const [isLoaded, setIsLoaded] = useState(true);
 
-   const getExplorerData = async () => {
-    try{
-        const response = await fetch(url);
-        const myData = await response.json();
-        setExplorerData(myData);
-        setIsLoaded(false);
-        console.log(myData);    
-    }catch(error){
-        console.log(error);
-    }
-   };
+    const getExplorerData = async () => {
+        try {
+            const response = await fetch(url);
+            const myData = await response.json();
+            setExplorerData(myData);
+            setIsLoaded(false);
+            console.log(myData);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-   //getExplorerData(url);
+    //getExplorerData(url);
 
-   useEffect(() => {
-    getExplorerData();
-   }, []);
+    useEffect(() => {
+        getExplorerData();
+    }, []);
 
     const initialCurrentLocation = {
         streetName: "Morristown",
@@ -301,34 +301,34 @@ const Home = ({ navigation }) => {
 
 
     function renderRestaurantList() {
-        const renderMainTileItems = ({item}) => <RenderTileItem item={item} navigation={navigation}  currentLocation={currentLocation} />;
+        const renderMainTileItems = ({ item }) => <RenderTileItem item={item} navigation={navigation} currentLocation={currentLocation} />;
 
         return (
             <FlatList
                 data={explorerData}
-                renderItem= {renderMainTileItems}
-                keyExtractor={item => `${item.title}`}
+                renderItem={renderMainTileItems}
                 contentContainerStyle={{
                     paddingHorizontal: SIZES.padding * 2,
                     paddingBottom: 30
                 }}
+                keyExtractor={(item, index) => index.toString()}
             />
         )
     }
-   
+
     return (
         <SafeAreaView style={styles.container}>
-            { isLoaded ? (
+            {isLoaded ? (
                 <View style={styles.loader}>
-                    <ActivityIndicator size="large" color="#0000ff"/>
+                    <ActivityIndicator size="large" color="#0000ff" />
                 </View>
-                ):(
-                    [
+            ) : (
+                [
                     RenderHeader(currentLocation),
-                    renderMainCategories(),
+
                     renderRestaurantList()
-                    ]
-                )
+                ]
+            )
             }
         </SafeAreaView>
     )
